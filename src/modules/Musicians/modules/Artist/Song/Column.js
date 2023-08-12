ec282a6ac122094f0daeb14f6786768d3865b10f
Gbenga Fagbemim <gbengaobafemi1@gmail.com>
2023-08-12 10:39:34 +0100

    Add Column.js

---
+++ src/modules/Musicians/modules/Artist/Song/Column.js
@@ -0,0 +1,6 @@
+export const columns = [
+  { title: "Name", field: "name" },
+  { title: "Genre", field: "genre.name" },
+  { title: "Duration", field: "duration" },
+  { title: "Stream", field: "stream" },
+];
